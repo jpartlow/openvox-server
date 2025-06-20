23e5aec9cd6d2da90fe298a922f81f9491eb3f4d
Josh Partlow <jpartlow@glatisant.org>
2025-06-20 12:26:11 -0700

    (maint) Clean up two static analysis warnings
    ...in client_may_use_external_cert_chains (ambiguous '/').

--- acceptance/suites/tests/https/client_may_use_external_cert_chains.rb
+++ acceptance/suites/tests/https/client_may_use_external_cert_chains.rb
@@ -80,7 +80,7 @@ EOF
   end
 
   report_content = on(master, "cat #{directory_to_serve}/#{master}").stdout.chomp
-  assert_match /(un)?changed/, report_content
+  assert_match(/(un)?changed/, report_content)
 
   system_ssl_tmpdir = master.tmpdir('system_ssl_store')
   script_location = system_ssl_tmpdir + '/connection_test.rb'
@@ -101,5 +101,5 @@ EOF
 
   create_remote_file(master, script_location, test_script)
   status = on(master, "/opt/puppetlabs/server/apps/puppetserver/bin/puppetserver ruby #{script_location}").stdout.chomp
-  assert_match /.*200$/, status
+  assert_match(/.*200$/, status)
 end
